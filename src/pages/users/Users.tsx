4336d69b59749b990ab600fedc9f5bd126c29ade
Vaibhav Tiwari <vishutiwari99@gmail.com>
2025-05-03 06:36:31 +0530

    Add Users Table

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,17 +1,65 @@
 import { RightOutlined } from "@ant-design/icons";
-import { Breadcrumb } from "antd";
+import { useQuery } from "@tanstack/react-query";
+import { Breadcrumb, Space, Table, TableProps } from "antd";
 import { Link } from "react-router-dom";
-
+import { getUsers } from "../../http/api";
+import { User } from "../../types";
+const columns: TableProps<User>["columns"] = [
+  {
+    title: "ID",
+    dataIndex: "id",
+    key: "id",
+    render: (text: string) => <span>{text}</span>,
+  },
+  {
+    title: "Name",
+    dataIndex: "firstName",
+    key: "firstName",
+    render: (_text: string, record: User) => (
+      <span>
+        {record.firstName} {record.lastName}
+      </span>
+    ),
+  },
+  {
+    title: "Role",
+    dataIndex: "role",
+    key: "role",
+    render: (text: string) => <span>{text}</span>,
+  },
+  {
+    title: "Email",
+    dataIndex: "email",
+    key: "email",
+    render: (text: string) => <span>{text}</span>,
+  },
+];
 const Users = () => {
+  const {
+    data: users,
+    error,
+    isError,
+    isLoading,
+  } = useQuery({
+    queryKey: ["users"],
+    queryFn: () => {
+      return getUsers().then((response) => response.data);
+    },
+  });
   return (
     <>
-      <Breadcrumb
-        separator={<RightOutlined />}
-        items={[
-          { title: <Link to="/dashboard">Dashboard</Link> },
-          { title: "Users" },
-        ]}
-      />
+      <Space direction="vertical" size={"large"} style={{ width: "100%" }}>
+        <Breadcrumb
+          separator={<RightOutlined />}
+          items={[
+            { title: <Link to="/dashboard">Dashboard</Link> },
+            { title: "Users" },
+          ]}
+        />
+        {isLoading && <div>Loading...</div>}
+        {isError && <div>{error.message}</div>}
+        <Table<User> columns={columns} dataSource={users} />
+      </Space>
     </>
   );
 };
--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,12 @@ export type Credentials = {
   email: string;
   password: string;
 };
+
+export type User = {
+  id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  role: string;
+  createdAt: string;
+};
